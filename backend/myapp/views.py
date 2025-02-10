a0db4b0e1c39accd34cf150ca496a8964969feb5
akash-deep-patel <73830546+akash-deep-patel@users.noreply.github.com>
2025-02-10 14:04:20 +0530

    cleanup

--- backend/myapp/views.py
+++ backend/myapp/views.py
@@ -2,14 +2,10 @@
 from django.http import JsonResponse
 from .models import Doctor, Patient, Appointment
 from django.views.decorators.csrf import csrf_exempt
-import json
 from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from django.conf import settings
-import msal
 import requests
-import os
 
 def home(request):
     return render(request, 'home.html')
